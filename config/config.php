6a1f5e92a3a14e0090a1832b9a2584f671753b57
Dongasai <1514582970@qq.com>
2018-06-25 09:35:47 +0800

    01

--- config/config.php
+++ config/config.php
@@ -1,11 +1,6 @@
 <?php
 
 return [
-    'router' => [
-        'map' => [
-            'mapdemo-demo' => 'demo-mapdemo'
-        ]
-    ],
     'overtime' => 10,
     'config_init' => false,
     'server_reg' => true,
