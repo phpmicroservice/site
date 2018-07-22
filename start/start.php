5b4a57b2ec43ea25328ca1858eb71f9ad2c19103
Dongasai <1514582970@qq.com>
2018-07-22 08:59:58 +0800

    2018年7月22日08:59:45 更改运行模式

--- start/start.php
+++ start/start.php
@@ -28,7 +28,7 @@ $loader->register();
 //$cbcu->testUpdate(ROOT_DIR.'/db/common.php');
 //echo "数据库升级完毕! \n";
 # 开启 服务
-$server = new \pms\Server('0.0.0.0', 9502, SWOOLE_BASE, SWOOLE_SOCK_TCP, [
+$server = new \pms\Server('0.0.0.0', 9502, SWOOLE_PROCESS, SWOOLE_SOCK_TCP, [
     'daemonize' => false,
     'reload_async' => false,
     'task_worker_num' => 4,
