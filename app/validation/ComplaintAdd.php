386910a95605a2009b66a623d5565010b0b83dc7
Dongasai <1514582970@qq.com>
2018-08-10 09:21:04 +0800

    5

--- app/validation/ComplaintAdd.php
+++ app/validation/ComplaintAdd.php
@@ -12,12 +12,16 @@ class ComplaintAdd extends Validation
         $this->add_required(['type', 'linkman', 'tel', 'email', 'title', 'content'], [
             'message' => 'required'
         ]);
-        $this->add_in('type',[
-            'domain'=>[
+        # 验证码
+        $this->add_required(['captcha_value', 'captcha_identifying'], [
+            'message' => 'required'
+        ]);
+        $this->add_in('type', [
+            'domain' => [
                 'person', 'company'
             ]
         ]);
-        $this->add_email('email',[
+        $this->add_email('email', [
             'message' => 'required'
         ]);
 
@@ -30,5 +34,22 @@ class ComplaintAdd extends Validation
             'max' => 1000,
         ]);
         return parent::initialize();
+    }
+
+    # 投诉验证码
+    public function beforeValidation1($data)
+    {
+        # 验证码
+        $this->add_Validator('captcha_value', [
+            'name' => Validation\Validator\ServerAction::class,
+            'server_action' => 'validation@/server/true_check',
+            'data' => [
+                "sn" => 'site',
+                "operation" => 'complaint',
+                "value" => $data['captcha_value'],
+                "identifying" => $data['captcha_identifying']
+            ],
+            'message' => 'captcha'
+        ]);
     }
 }
